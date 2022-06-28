b0eb5ddb34974e2548f953cdb8e522da854b250b
Naveen Kuragayala <82062695+knaveen96@users.noreply.github.com>
2022-06-28 19:13:12 -0400

    Time: 0 ms (100.00%), Space: 41.6 MB (65.48%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -0,0 +1,100 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+        
+//         if(head == null) return null;
+//         if(head.next == null) return head;
+        
+//         ListNode p = head;
+//         ListNode q = head;
+//         ListNode temp = head;
+//         ListNode new_head = p.next;
+        
+//         while(p != null) {
+            
+//             q = p.next;
+//             temp = q.next;
+//             q.next = p;
+            
+//             if(temp == null || temp.next == null) {
+//                 p.next = temp;
+//                 break;
+//             }
+ 
+//             p.next = temp.next;
+            
+//             p = temp;
+//         }
+        
+//         return new_head;
+//     }
+// }
+
+
+
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        
+        if(head == null) return null;
+        if(head.next == null) return head;
+        
+        ListNode p = head;
+        ListNode q = head.next;
+        ListNode temp = q.next;
+        ListNode new_head = p.next;
+        
+        while(p != null) {
+            
+            q.next = p;
+            if(temp == null || temp.next == null) {
+                p.next = temp;
+                break;
+            }
+                
+            p.next = temp.next;
+            
+            p = temp;
+            q = p.next;
+            temp = q.next;
+        }
+        
+        return new_head;
+    }
+}
+
+
+/*
+
+1-2-3-4-5-6-7-8
+
+1 points to 3 and 2 points to 1
+2-1-3-4-5-6-7-8
+
+1 points to 4 and 3 points to 5
+2-1-4-3-5-6-7-8
+
+
+
+2-1-4
+
+
+
+1-4
+2-1
+4-3
+
+2-1-4-3
+
+*/
+
+
