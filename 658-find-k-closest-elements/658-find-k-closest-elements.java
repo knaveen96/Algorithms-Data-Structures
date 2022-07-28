a5dbc9ece6907f240ce2518d1b30c53726d23719
Naveen Kuragayala <82062695+knaveen96@users.noreply.github.com>
2022-07-27 20:09:46 -0400

    Time: 6 ms (77.21%), Space: 62.7 MB (44.69%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -90,49 +90,103 @@
 */
 
 
-class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         
-        List<Integer> res = new ArrayList<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                if(Math.abs(a-x) == Math.abs(b-x)) {
-                    return b - a;
-                } else {
-                    return Math.abs(b-x) - Math.abs(a-x);
-                }
-            }
-        });
+//         List<Integer> res = new ArrayList<>();
+//         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<>() {
+//             @Override
+//             public int compare(Integer a, Integer b) {
+//                 if(Math.abs(a-x) == Math.abs(b-x)) {
+//                     return b - a;
+//                 } else {
+//                     return Math.abs(b-x) - Math.abs(a-x);
+//                 }
+//             }
+//         });
         
-        for(int i = 0 ; i < arr.length ; i++) {
-            maxHeap.add(arr[i]);
-            if(maxHeap.size() > k) {
-                maxHeap.poll();
-            }
-        }
+//         for(int i = 0 ; i < arr.length ; i++) {
+//             maxHeap.add(arr[i]);
+//             if(maxHeap.size() > k) {
+//                 maxHeap.poll();
+//             }
+//         }
                 
-        while(maxHeap.size() > 0) {
-            res.add(maxHeap.poll());
-        }
+//         while(maxHeap.size() > 0) {
+//             res.add(maxHeap.poll());
+//         }
         
-        Collections.sort(res);
+//         Collections.sort(res);
         
-        return res;
-    }
-}
+//         return res;
+//     }
+// }
 
 
 /*
 
+time : O(N log k) + O(k log k)
+
 arr = [1,2,3,4,5], k = 4, x = 3
 
 
+ 0 1 2 3 4 
+[1,2,3,4,5]
 
 
+
+low = 0
+high = 4
+
+mid = 2
+
+(3-3) = 0
 
 */
 
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+     
+        List<Integer> res = new ArrayList<>();
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(arr[mid] > x) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        left = left - 1;
+        right = left + 1;
+        
+        while(right - left - 1 < k) {
+            
+            if(left == -1) {
+                right = right + 1;
+            }
+            else if(right == arr.length) {
+                left = left - 1;
+            }
+            else if(Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+                left = left - 1;
+            }
+            else {
+                right = right + 1;
+            }
+        }
+        
+        for(int i = left + 1 ; i < right ; i++) {
+            res.add(arr[i]);
+        }
+        return res;
+        
+    }
+}
 
 
 
