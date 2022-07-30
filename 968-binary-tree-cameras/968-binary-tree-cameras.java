c5c2478a3d781f1d55d6cf21c97fc84476aa0bc5
Naveen Kuragayala <82062695+knaveen96@users.noreply.github.com>
2022-07-30 12:06:38 -0400

    Time: 1 ms (80.89%), Space: 44.4 MB (11.80%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -0,0 +1,82 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int cameras = 0;
+    public int minCameraCover(TreeNode root) {
+        
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        if(dfs(root) == 0) cameras += 1;
+        return cameras;
+    }
+    
+    public int dfs(TreeNode root) {
+        
+        if(root == null) return 1;
+        
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        
+        if(left == 0 || right == 0) {
+            cameras++;
+            return 2;
+        }
+        if(left == 1 && right == 1) {
+            return 0;
+        }
+        if(left == 2 || right == 2) {
+            return 1;
+        }
+        return -1;
+    }
+    
+}
+
+
+/*
+
+
+[0,null,0,null,0,null,0]
+
+                        0
+                         \
+                          0
+                           \
+                            2
+                             \
+                              0
+
+[0,0,0,0,0,null,null,0,0,0,0,null,null,null,null,null,null,0]
+
+
+                 2
+              /     \
+             0       0
+            /\       
+           2  2     
+          /\  /\    
+    0    0 2  0 0  
+           /
+          0 
+          
+         
+  1 - monitored
+  2 - camera
+  0 - not monitored
+  
+
+
+*/
