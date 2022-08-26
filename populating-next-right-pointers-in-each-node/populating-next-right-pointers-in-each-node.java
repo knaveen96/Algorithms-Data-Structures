82afa179c905ba3d602da602df154a00dd585ec6
Naveen Kuragayala <82062695+knaveen96@users.noreply.github.com>
2022-08-26 13:12:59 -0400

    Time: 0 ms, Memory: 46.7 MB - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -21,27 +21,51 @@ class Node {
 };
 */
 
+//class Solution {
+    //O(N) space
+//     public Node connect(Node root) {
+     
+//         if(root == null) return null;
+//         Queue<Node> queue = new LinkedList<>();
+//         queue.add(root);
+        
+//         while(!queue.isEmpty()) {
+//             int size = queue.size();
+            
+//             for(int i = 0 ; i < size ; i++) {
+//                 Node curr = queue.poll();
+//                 if(i < size - 1) curr.next = queue.peek();
+                
+//                 if(curr.left != null) {
+//                     queue.add(curr.left);
+//                 }
+//                 if(curr.right != null) {
+//                     queue.add(curr.right);
+//                 }
+//             }
+//         }
+//         return root;
+//     }
+    
+    
+    
 class Solution {
     public Node connect(Node root) {
-     
-        if(root == null) return null;
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
         
-        while(!queue.isEmpty()) {
-            int size = queue.size();
+        if(root == null) return null;
+        Node leftNode = root;
+        
+        while(leftNode.left != null) {
+            Node head = leftNode;
             
-            for(int i = 0 ; i < size ; i++) {
-                Node curr = queue.poll();
-                if(i < size - 1) curr.next = queue.peek();
-                
-                if(curr.left != null) {
-                    queue.add(curr.left);
+            while(head != null) {
+                head.left.next = head.right;
+                if(head.next != null) {
+                    head.right.next = head.next.left;
                 }
-                if(curr.right != null) {
-                    queue.add(curr.right);
-                }
+                head = head.next;
             }
+            leftNode = leftNode.left;
         }
         return root;
     }
