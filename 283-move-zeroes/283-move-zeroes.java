222da1a84c8b5f08c1a743e240b6198f59ed3f57
Naveen Kuragayala <82062695+knaveen96@users.noreply.github.com>
2022-08-29 14:14:39 -0400

    Time: 60 ms (9.68%), Space: 54.3 MB (76.81%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+     
+        int left = 0;
+        int right = 0;
+        
+        while(right < nums.length) {
+            
+            while(left < nums.length && nums[left] != 0) {
+                left++;
+            }
+            
+            right = left;
+            
+            while(right < nums.length && nums[right] == 0) {
+                right++;
+            }
+            
+            if(left < nums.length && right < nums.length) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+            
+        }
+    }
+}
+
+/*
+
+[0,0,0,0]
+ l
+         r
+
+
+ 0 1 2 3 4
+[1,3,0,0,12]
+     l
+          r
+ 
+
+- left should point the first zero
+- right should point non-zero
+
+
+
+[1,0,0,2,3,0]
+   l
+       r
+
+
+
+
+*/
