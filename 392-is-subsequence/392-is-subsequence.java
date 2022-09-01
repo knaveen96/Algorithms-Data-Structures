e73f5f670db5aa094183548b619f77c043d5bc2b
Naveen Kuragayala <82062695+knaveen96@users.noreply.github.com>
2022-09-01 11:31:01 -0400

    Time: 8 ms (5.71%), Space: 42.3 MB (23.97%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,129 @@
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+        
+//         HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
+//         int num = 1;
+//         int[] arr = new int[s.length()];
+//         int i = 0;
+
+//         for(char c : t.toCharArray()){
+//             if(!map.containsKey(c)){
+//                 ArrayList<Integer> indices = new ArrayList<Integer>();
+//                 indices.add(num);                
+//                 map.put(c, indices);
+//             }
+                
+//             else{
+//                 ArrayList<Integer> arrList = new ArrayList<>();
+//                 map.get(c).add(num);
+//             }
+
+//             num++;
+//         }
+        
+//         Integer currMatchIndex = -1;
+//         for (char letter : s.toCharArray()) {
+            
+//             if (!map.containsKey(letter))
+//                 return false;
+
+//             boolean isMatched = false;
+
+//             for (Integer matchIndex : map.get(letter)) {
+//                 if (currMatchIndex < matchIndex) {
+//                     currMatchIndex = matchIndex;
+//                     isMatched = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!isMatched)
+//                 return false;
+//         }
+
+//         return true;
+//     }
+// }
+
+// "abc"
+// "ahbgdc"
+
+// a 1
+// h 2
+// b 3
+// g 4
+// d 5
+// c 6
+
+
+
+/*
+
+s = "abc"
+t = "ahbgdc"
+
+012345
+ahbgdc
+^ 
+
+
+
+abc
+  ^
+
+
+
+
+*/
+
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        int sp = 0;
+        int tp = 0;
+        
+        if(t.length() < s.length()) return false;
+        
+        while(sp < s.length()) {
+            
+            if(tp >= t.length()) {
+                return false;
+            }
+            char tChar = t.charAt(tp);
+            char sChar = s.charAt(sp);
+            
+            
+            if(tChar == sChar) {
+                tp++;
+                sp++;
+            }
+            else if(tChar != sChar) {
+                tp++;
+            }
+            
+        }
+        return true;
+        
+    }
+}
+
+
+/*
+
+aa
+^
+
+zz
+  ^
+
+*/
+
+
+
+
+
+
+
+
+
