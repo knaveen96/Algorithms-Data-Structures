9903837bd524988a6886a94ca810d1f3a14fe128
Naveen Kuragayala <82062695+knaveen96@users.noreply.github.com>
2022-09-13 14:26:20 -0400

    Time: 181 ms (18.09%), Space: 87 MB (39.71%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.java
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.java
@@ -1,40 +1,21 @@
 class OrderedStream {
     int ptr;
-    String[] arr;
+    String[] res;
+    
     public OrderedStream(int n) {
-        ptr = 1;
-        arr = new String[n + 1];
+        ptr = 0;
+        res = new String[n];
     }
     
-    public List<String> insert(int idKey, String value) {
-        arr[idKey] = value;
+    public List<String> insert(int id, String value) {
         List<String> list = new ArrayList<>();
         
-        while(ptr < arr.length && arr[ptr] != null) {
-            list.add(arr[ptr++]);
+        res[id - 1] = value;
+        while (ptr < res.length && res[ptr] != null) {
+            list.add(res[ptr]);
+            ptr++;
         }
+        
         return list;
     }
 }
-
-/**
- * Your OrderedStream object will be instantiated and called as such:
- * OrderedStream obj = new OrderedStream(n);
- * List<String> param_1 = obj.insert(idKey,value);
- */
-
-
-
-/*
-
-5, e
-int ptr = 4
-
-[3, "ccccc"], [1, "aaaaa"], [2, "bbbbb"], [5, "eeeee"], [4, "ddddd"]
-
-
-a b c
-
-
-
-*/
