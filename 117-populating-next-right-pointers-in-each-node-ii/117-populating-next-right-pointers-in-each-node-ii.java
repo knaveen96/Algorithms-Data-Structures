59265355a63028a4c2ceecd1cc190437e0f112d9
Naveen Kuragayala <82062695+knaveen96@users.noreply.github.com>
2022-09-27 23:44:19 -0400

    Time: 1 ms (81.11%), Space: 44.8 MB (63.06%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -103,27 +103,83 @@ class Node {
 
 
 
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root == null) return null;
+//         Queue<Node> queue = new LinkedList<>();
+//         queue.add(root);
+        
+//         while(!queue.isEmpty()) {
+//             int size = queue.size();
+//             for(int i = 0 ; i < size ; i++) {
+//                 Node curr = queue.poll();
+//                 if(i < size - 1) curr.next = queue.peek();
+//                 if(curr.left != null) queue.add(curr.left);
+//                 if(curr.right != null) queue.add(curr.right);
+//             }
+//         }
+//         return root;
+//     }
+// }
+
+
 class Solution {
     public Node connect(Node root) {
         if(root == null) return null;
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
+        Node curr = root;
         
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i = 0 ; i < size ; i++) {
-                Node curr = queue.poll();
-                if(i < size - 1) curr.next = queue.peek();
-                if(curr.left != null) queue.add(curr.left);
-                if(curr.right != null) queue.add(curr.right);
+        while(curr != null) {
+            Node dummy = new Node(-1);
+            Node ptr = dummy;
+            Node head = curr;
+            
+            while(head != null) {
+                if(head.left != null) {
+                    ptr.next = head.left;
+                    ptr = ptr.next;
+                }
+
+                if(head.right != null) {
+                    ptr.next = head.right;
+                    ptr = ptr.next;
+                }
+                head = head.next;
             }
-        }
+            curr = dummy.next;
+        }        
         return root;
     }
 }
 
-/*
 
+/*
+                    1
+                2       3
+            4               5
+
+
+while(curr.left != null || curr.right != null) {
+    Node prev = curr;
+    while(curr != null) {
+        if(curr.left != null) curr.left.next = curr.right;
+        if(curr.next != null && curr.right != null)
+            if(curr.next.left != null) 
+                curr.right.next = curr.next.left;
+            else 
+                curr.right.next = curr.next.right;
+        curr = curr.next;
+    }
+    curr = prev;
+    if(curr.left != null) curr = curr.left;
+    else curr = curr.right;
+}
+
+
+            1
+                   3
+                4     5
+                
+                
 
 
 */
